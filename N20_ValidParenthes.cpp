1d678837a1c09268d71e8cb534e63a4d29715c09
VladimirPetrov <31584866+vladimir1254@users.noreply.github.com>
2020-02-08 17:24:00 +0300

    Create N20_ValidParenthes.cpp

---
+++ N20_ValidParenthes.cpp
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/valid-parentheses/
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0;i<s.size();i++)
+        {
+            if(s[i]=='(' || s[i]=='['|| s[i]=='{')
+            st.push(s[i]);
+            else
+            {
+                if(!st.empty())
+                {
+                if((st.top()=='[' && s[i]!=']') || (st.top()=='('&&s[i]!=')')||(st.top()=='{'&& s[i]!='}'))
+                     return false;
+                st.pop();
+                }
+                else return false;
+            }
+        }
+        if(st.empty())
+        return true;    
+        else
+            return false;
+    }
+};
