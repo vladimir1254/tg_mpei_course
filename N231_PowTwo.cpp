f90dd45f1f3824cc572c316205737e6e0844d945
VladimirPetrov <31584866+vladimir1254@users.noreply.github.com>
2020-02-20 20:44:02 +0300

    Create N231_PowTwo.cpp

---
+++ N231_PowTwo.cpp
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/power-of-two/
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        if(n<=0) return false;
+        while(n>0)
+        {
+            if(n%2==0 || n==1)
+            n/=2;
+            else
+                return false;
+        }
+        return true;
+    }
+};
